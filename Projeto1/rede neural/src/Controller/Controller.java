f2a9ebb01ba0544214a87f80296f5c55631a22c8
diogolimamarques <diogolimamarques@hotmail.com>
2018-05-09 09:58:00 -0300

    Create Controller.java

---
+++ Projeto1/rede neural/src/Controller/Controller.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Team;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author diogo
+ */
+public class Controller {
+    
+    ArrayList<Team> listaTimes = new ArrayList<Team>();
+
+    public ArrayList<Team> getListaTimes() {
+        return listaTimes;
+    }
+
+    public void setListaTimes(ArrayList<Team> listaTimes) {
+        this.listaTimes = listaTimes;
+    }
